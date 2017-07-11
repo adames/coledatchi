49338706d2a1f5f658d16b5040c0cbcb7a7bf2f2
adames <adameshh@gmail.com>
2017-07-11 17:30:41 -0400

    made new, create, show pages for users

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,26 @@
 class PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(name: pet_params[:name])
+    @pet.user = current_user
+    if @pet.save
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+
+private
+
+  def pet_params
+    params.require(:pet).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
+  root to: 'users#home'
+
   get '/login', to: 'sessions#new', as: 'login'
   # log in user
   post '/sessions', to: 'sessions#create', as: 'sessions'
   # log out user
   delete '/sessions', to: 'sessions#destroy', as: 'logout'
 
-  root to: 'users#index'
+
+  resources :users, only: [:new, :create, :show]
 
 
-  resources :users
+  resources :pets, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
