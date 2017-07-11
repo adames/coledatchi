49502dbe2ee82b8f9c64cadd3c4388d9dd6b12d4
adames <adameshh@gmail.com>
2017-07-11 12:08:33 -0400

    added faker and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710225921) do
+ActiveRecord::Schema.define(version: 20170711132610) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170710225921) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require_relative '../config/environments/development.rb'
+
+users = []
+10.times do
+  users << User.create(username: Faker::HarryPotter.character)
+end
+
+pets = []
+10.times do |i|
+  pets << Pet.create(name: Faker::Pokemon.name, user_id: users[i].id)
+end
+
+10.times do |i|
+  Story.create(history: [], pet_id: pets[i].id)
+end
+
+
+
+  # create_table "events", force: :cascade do |t|
+  #   t.string "name"
+  #   t.string "description"
+  #   t.string "results"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+  #
+  # create_table "pets", force: :cascade do |t|
+  #   t.string "name"
+  #   t.integer "happiness", default: 10
+  #   t.integer "hygiene", default: 10
+  #   t.integer "hunger", default: 0
+  #   t.integer "user_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+  #
+  # create_table "stories", force: :cascade do |t|
+  #   t.string "history"
+  #   t.integer "pet_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+  #
+  # create_table "users", force: :cascade do |t|
+  #   t.string "username"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
