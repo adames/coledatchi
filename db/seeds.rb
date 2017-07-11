8ce8716e9b3607b372afe16fd03f6553680eb98f
github username <github email address>
2017-07-11 18:29:33 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,10 +22,19 @@ stories = []
 end
 
 
-# Event.create()
+# New Event's should be in PAST tense
+
 Event.create(name: "Walk your pet", description: "You walked your pet", "results" => {happiness: 1, hunger: 1})
 Event.create(name: "Wash your pet", description: "You gave your pet a bath", "results" => {hygiene: 5})
 Event.create(name: "Feed your pet", description: "You fed your pet", "results" => {hunger: -5})
+Event.create(name: "Volcanoe", description: "A volcanoe exploded nearby.  It scared your pet.  Your pet also became strangely hungry", "results" => {happiness: -4, hunger: 1})
+Event.create(name: "Mugged", description: "Your pet got ambushed by a roaving band of rogue pets.  It got mugged", "results" => {happiness: -7, hygiene: -3})
+Event.create(name: "Mugged but not", description: "Your pet got ambushed by a roaving band of rogue pets.  They try to mug your pet but they get mugged themselves", "results" => {happiness: 7, hygiene: -3})
+Event.create(name: "Top Chef", description: "Your pet watched an episode of Top Chef", "results" => {happiness: 5, hunger: -3})
+Event.create(name: "Haircut", description: "Your pet gets a haircut", "results" => {happiness: 4})
+Event.create(name: "Hotdate", description: "Your pet has a hot date planned", "results" => {happiness: 8})
+Event.create(name: "Play", description: "You rubbed your pet's belly", "results" => {happiness: 3})
+Event.create(name: "Sick", description: "You pet fell sick for a bit but is better now", "results" => {happiness: -5})
 
 
   # create_table "events", force: :cascade do |t|
