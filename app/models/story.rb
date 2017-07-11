2b815487769197ce2497dfbb688a1a7939f0fb63
adames <adameshh@gmail.com>
2017-07-11 09:52:29 -0400

    added new events

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,8 +2,13 @@ class Story < ApplicationRecord
   belongs_to :pet
   has_many :events
 
+  def update_story
+    #this method should update stories on a pet depending on time
+  end
 
-
-
+  def random_event(requirements)
+    # given a set of requirements, it serves a random, appropriate event object.
+    
+  end
 
 end
