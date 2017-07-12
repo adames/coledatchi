8cde7dedd9d29c0aca01a556b0538b96d1162215
adames <adameshh@gmail.com>
2017-07-12 13:52:30 -0400

    added personality generator"

--- app/models/personality.rb
+++ app/models/personality.rb
@@ -1,5 +1,11 @@
 class Personality < ApplicationRecord
   belongs_to :pet
 
+  @@types = ['brave', 'curious', 'social']
+
+  def self.generate_random(pet_obj)
+    self.new(type: rand(@@types.sample, pet_id: pet_obj.id)
+  end
+
   # types I'm thinking so far = ['brave', 'curious', 'social']
 end
