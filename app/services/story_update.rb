12372600a57536b66e7e08de3a52198ab71848b5
github username <github email address>
2017-07-13 15:26:14 -0400

    everything works

--- app/services/story_update.rb
+++ app/services/story_update.rb
@@ -13,10 +13,6 @@ class StoryUpdate
   end
 
 
-
-
-
-
   def update_pet_only_from_time
     #this should update a pet's hunger, happiness, etc... based on time passed since last updated
     @last_updated = self.pet.updated_at.localtime
@@ -26,21 +22,23 @@ class StoryUpdate
     self.pet.hygiene -= @hours_passed / 2
     self.pet.hunger += @hours_passed
 
-    update_story_random(@minutes_passed)
+    update_story_random(@minutes_passed, @last_updated)
 
     pet.save
     pet.story.save
 
   end
 
-  def update_story_random(minutes)
+  def update_story_random(minutes, last_updated)
     #this method should update stories on a pet depending on time
 
-    @ticks = minutes - 10
+    @ticks = (minutes * 60) - 10
+    backwards_history = []
     while @ticks > 0
       @random_event = RandomEvent.find( 1 + rand(RandomEvent.all.size))
       puts "#{@random_event.description}"
-      pet.story.history << "#{@random_event.description} on #{Time.now.strftime("%A, %d on %I:%M%p")}"
+      last_updated -= @ticks
+      backwards_history << "#{@random_event.description} on #{last_updated.strftime("%A, %d on %I:%M%p")}"#{Time.now.strftime("%A, %d on %I:%M%p")}
       unless @random_event.results[:happiness] == nil
          pet.happiness += @random_event.results[:happiness]
       end
@@ -50,10 +48,16 @@ class StoryUpdate
       unless @random_event.results[:hunger] == nil
         pet.hunger += @random_event.results[:hunger]
       end
-      # pet.save
-      # pet.story.save
-      @ticks -= (5 + rand(1..@ticks))
+      @ticks -= (300 + rand(1..@ticks))
     end
+
+    backwards_history.reverse.each do |event|
+      pet.story.history << event
+    end
+
+    pet.save
+    pet.story.save
+
   end
 
   def update_story_personality
