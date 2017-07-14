88ab1036bdab00260b5897f98cef83f1f7fb9f5a
adames <adameshh@gmail.com>
2017-07-14 14:53:47 -0400

    fixed merge conflict in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714154026) do
+ActiveRecord::Schema.define(version: 20170714174946) do
 
   create_table "histories", force: :cascade do |t|
     t.string "name"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170714154026) do
     t.integer "species_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["species_id"], name: "index_pets_on_species_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
